c422bb11e3371692e82575ffc10a06c960b4a670
Zakesde-prince <prince.feela2905@gmail.com>
2025-07-27 13:05:46 +0200

    .

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,10 +1,8 @@
-import Layout from '../components/layouts/Layout';
-
-export default function Home() {
+const Home = () => {
   return (
-    <Layout>
-      <h1>Welcome to Splash App!</h1>
-      {/* Your main page content */}
-    </Layout>
-  )
+   <div>
+    <h1> Welcome to Splash App </h1>
+   </div>
 }
+
+export default Home;
