68e0361eb46790612c9b69ef2e71b2914b7d55fe
Zakesde-prince <prince.feela2905@gmail.com>
2025-07-27 19:45:48 +0200

    .

--- alx-project-0x03/interface/index.ts
+++ alx-project-0x03/interface/index.ts
@@ -1,18 +1,15 @@
 // interface/index.ts
 
-// From pages/index.tsx
 export interface PageRouteProps {
   pageRoute: string;
 }
 
-// From components/common/Button.tsx
 export interface ButtonProps {
   action: () => void;
   buttonLabel: string;
   buttonBackgroundColor: "blue" | "green" | "orange";
 }
 
-// From components/layouts/Layout.tsx
 export interface LayoutProps {
   children: React.ReactNode;
 }
